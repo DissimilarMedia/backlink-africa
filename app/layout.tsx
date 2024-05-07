467a4404389ccbbc7b0cc1a03d463b55664d8ee4
pascal <pascal@marascientific.org>
2024-05-07 23:41:06 +0300

    adds gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,8 @@ export const metadata = {
   description: 'We are a one-stop shop for advertisers interested in African websites, and content creators interested in monetizing their website. Visit our website if you are an advertiser interested in link building, sponsored content posts or are a publisher interested in making money from your content.',
 }
 
+
+
 export default function RootLayout({
   children,
 }: {
@@ -31,6 +33,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-7JZZ8HD713"></script>
+      <script>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+        gtag('config', 'G-7JZZ8HD713');`}
+      </script>
       <body className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-200 text-gray-200 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden">
           <Header />
